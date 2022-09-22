fcfdcaf3b66a47e3e21ad3b8d6c87fb0d340ddfd
Rakesh-123-cryp <roketraj03@gmail.com>
2022-09-22 13:15:36 +0530

    added all the vendors

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template,request,redirect,url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 def initial():
-    return render_template(r"User/rakesh/Desktop/Kivy_app/Mini-Project/index1.html")
+    session["login_status"] = False
+    return render_template(r"index.html", status = session["login_status"])
 
 @app.route("/login")
 def index():
-    if request.method == "GET":
-        var1 = request.args.get('email')
-        return "<h1>var1</h1>"#redirect(url_for("redicrected_page" , var = var1))
+    if request.method == "POST":
+        mail = request.forms['email']
+        passwd = request.forms['password']
+        
+        return redirect("/redicrected" , var = var1)
 @app.route("/redirected")
 def redicrected_page(var):
     return var
