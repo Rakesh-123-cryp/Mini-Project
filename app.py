02a6d84c8f2b01c916640cefd38564163118d69b
Rakesh-123-cryp <roketraj03@gmail.com>
2022-09-23 09:56:07 +0530

    changes with mohit

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
-from flask import Flask, render_template,request,redirect,url_for
-
+from flask import Flask, render_template,request,redirect,url_for,session
+import csv
 app = Flask(__name__)
-
+app.secret_key = "abcd"
 @app.route("/")
 def initial():
     session["login_status"] = False
-    return render_template(r"index.html", status = session["login_status"])
+    if session["login_status"] == True:
+        return render_template("index.html", status = session["login_status"])
+    return render_template("index.html", status = session["login_status"])
 
-@app.route("/login")
+@app.route("/login",methods = ["POST","GET"])
 def index():
-    if request.method == "POST":
-        mail = request.forms['email']
-        passwd = request.forms['password']
-        
-        return redirect("/redicrected" , var = var1)
-@app.route("/redirected")
+    if request.method == "POST" and request.form['submit'] == 'Login':
+        mail = request.form['email']
+        passwd = request.form['password']
+        with open ("files/customers.csv","r") as file:
+            r = csv.reader(file)
+            for i in r:
+                check = 0
+                if i[0] == mail and i[1] == password:
+                    check = 1
+                    break
+            if check == 0:
+                return redirect(url_for("/login" , auth = False))
+            session["login_status"] = True
+            return redirect(url_for("initial" , status = session['login_status']))
+    else:
+        return render_template("login.html")
+@app.route("/signup")
 def redicrected_page(var):
-    return var
+    if request.method == "POST" and request.form['submit'] == 'Login':
+        details = []
+
+        with open ("files/customers.csv","w+") as file:
+            w = csv.writer(file)
+            details.append(request.form['email'])
+            details.append(request.form['password'])
+            details.append(request.form['confirm_password'])
+            w.writerow(details)
 
 if(__name__ == "__main__"):
     app.run(debug = True)
