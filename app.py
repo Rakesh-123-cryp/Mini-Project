f55923e7b82ec04f7890a6dd4e5b0e9d8a3ddf53
mohitavva <40499116+mohitavva@users.noreply.github.com>
2022-09-27 19:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for,session
 import mysql.connector as mc
-
 my_db = mc.connect(user = "root" , host = "localhost" , password = "22072003Rr" , database = "MiniProject ")
 cur = my_db.cursor()
-
 app = Flask(__name__)
 app.secret_key = "abcd"
 
@@ -18,11 +16,12 @@ def initial():
     session["status"] = False
     return render_template("index.html", username = "Login")
 
-@app.route("/browse")
-def browse_page():
+
+@app.route("/<username>/browse")
+def browse_page(username):
     if session['status'] == False:
-        return redirect(url_for("index"))
-    return redirect(url_for("index"))
+        return redirect(url_for("browse_guest"))
+    return render_template('browse.html', username = username)
 
 
 @app.route("/<username>")
@@ -30,6 +29,7 @@ def logged(username):
     if session["status"] == False:
         return redirect(url_for("index"))
     return render_template("index.html" , username = username)
+
 
 @app.route("/login",methods = ["POST","GET"])
 def index():
@@ -42,12 +42,14 @@ def index():
             print(Id_s)
             if len(Id_s) == 0:
                 return redirect(url_for("wrongpass"))
+                return redirect(url_for("wrongpass"))
             else:
                 session["status"] = True
                 session["username"] = Id_s[0][0]
                 return redirect(url_for("logged" , username = Id_s[0][0]))
         else:
             return render_template("login.html")
+
 
 @app.route("/signup")
 def sign_up_page(var):
@@ -65,6 +67,7 @@ def sign_up_page(var):
             w.writerow(details)
         return redirect(url_for("logged" , username = details[0]))
 
+
 @app.route("/home")
 def home_button():
     if session["status"] == True:
@@ -81,6 +84,7 @@ def wrongpass():
             Id_s = cur.fetchall()
             print(Id_s)
             if len(Id_s) == 0:
+                return render_template("wrongpass.html")
                 return redirect(url_for("wrongpass"))
             else:
                 session["status"] = True
@@ -89,9 +93,9 @@ def wrongpass():
         else:
             return render_template("wrongpass.html")
 
+
 @app.route("/terms")
 def terms():
     return render_template("terms.html")
-
 if(__name__ == "__main__"):
     app.run(debug = True)
